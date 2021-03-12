a0f47ee009b30160ae1deecd5e73775aace66813
pritomdip <pritomdip010@gmail.com>
2021-03-12 19:15:29 +0600

    modified staff

--- database/migrations/2021_03_06_134810_create_staff_table.php
+++ database/migrations/2021_03_06_134810_create_staff_table.php
@@ -20,14 +20,14 @@ class CreateStaffTable extends Migration
             $table->string('father', 150)->nullable();
             $table->string('mother', 150)->nullable();
             $table->string('address', 150)->nullable();
-            $table->enum('marriage_status',[1, 0])->default(1);
+            $table->enum('marriage_status', [1, 0])->default(1);
             $table->string('chil_boy', 150)->nullable();
             $table->string('chil_girl', 150)->nullable();
             $table->string('joining_date', 150)->nullable();
             $table->string('office_phone', 150)->nullable();
             $table->string('personal_phone', 150)->nullable();
             $table->string('email', 150)->nullable();
-            $table->integer('Priority')->default(50);
+            $table->integer('serial')->default(50);
             $table->string('image')->default('avatar.png');
             $table->enum('status', ['a', 'd'])->default('a');
             $table->timestamps();
